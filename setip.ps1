0e2d74db23515e57bebb137ea4d1e700a83bf919
zhang <zhangwei_w8284@proware.net>
2016-08-12 15:08:17 +0800

    add netadapter command

---
+++ setip.ps1
@@ -0,0 +1,10 @@
+﻿
+$window = New-Object -ComObject WScript.shell
+$wmi = Get-NetAdapter
+foreach ($adapter in $wmi)
+{
+Write-Host $adapter.Name
+if ($adapter.status -eq 'Disabled' )
+{$window.popup($adapter.name,5,"interface",1+64)}
+
+}
