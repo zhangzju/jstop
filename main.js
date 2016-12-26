76e6b04371412614b5d9b4c90cffe4f36a7e2b98
zhangzju <zhangway19921221@163.com>
2016-12-27 01:21:24 +0800

    set layout

--- main.js
+++ main.js
@@ -2,15 +2,17 @@ const electron = require('electron')
 const {app} = require('electron')
 const path = require('path')
 const os = require('os')
+const ipc = require('electron').ipcMain
 
 const {BrowserWindow} = electron
 
 let mainWindow
+let topInfo = []
 
 function createWindow (){
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 1300, height: 640})
   mainWindow.loadURL('file://' + __dirname + '/index.html')
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
   mainWindow.on('closed', ()=>{
     mainWindow = null
   })
@@ -21,6 +23,7 @@ app.on('ready', function (){
   console.log(os.cpus().length)
   for (let cpu of os.cpus()){
     // console.log(cpu.model);
+
   }
 })
 
@@ -34,4 +37,8 @@ app.on('activate', () => {
   if (mainWindow === null) {
     createWindow()
   }
+})
+
+ipc.on('synchronous-message', function (event, arg) {
+  event.returnValue = 'pong'
 })
